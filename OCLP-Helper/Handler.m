4e9ded54cdbad256aacc53cbf574428bac8068ee
Mykola Grymalyuk <khronokernel@icloud.com>
2024-05-27 13:50:11 -0600

    Handler.m: Adjust centering logic

--- OCLP-Helper/Handler.m
+++ OCLP-Helper/Handler.m
@@ -53,7 +53,12 @@
     [progressBar startAnimation:nil];
     [window.contentView addSubview:progressBar];
 
-    [window center];
+
+    // Reference: https://stackoverflow.com/a/61174052
+    CGFloat xPos = NSWidth(window.screen.frame) / 2 - NSWidth(window.frame) / 2;
+    CGFloat yPos = NSHeight(window.screen.frame) / 2 - NSHeight(window.frame) / 2;
+
+    [window setFrame:NSMakeRect(xPos, yPos, NSWidth(window.frame), NSHeight(window.frame)) display:YES];
 }
 
 -(void)spawnProgressWindow {
